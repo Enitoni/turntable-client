4b06134c4ede1223674c42df133569f1a785acfc
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-06-15 16:09:18 -0500

    set up remix routes with some typed helpers

--- app/components/AppHeader.tsx
+++ app/components/AppHeader.tsx
@@ -1,13 +1,13 @@
-import { Link } from "@remix-run/react"
+import { LinkTyped } from "~/lib/remix-routes.tsx"
 import { AppLogo } from "./AppLogo.tsx"
 import { UserButton } from "./UserButton.tsx"
 
 export function AppHeader() {
 	return (
 		<header className="bg-darkish px-4 h-16 justify-between flex items-center">
-			<Link to="/">
+			<LinkTyped to="/">
 				<AppLogo className="h-10 w-auto" />
-			</Link>
+			</LinkTyped>
 			<UserButton />
 		</header>
 	)
---
+++ app/lib/remix-routes.tsx
@@ -0,0 +1,68 @@
+// it is best for your sanity that you ignore this file
+
+import {
+	Link,
+	type LinkProps,
+	NavLink,
+	type NavLinkProps,
+	useNavigate,
+	useParams,
+} from "@remix-run/react"
+import { $params, $path, type Routes, type RoutesWithParams } from "remix-routes"
+
+interface BaseLinkPropsWithoutParams<To extends keyof Routes> {
+	to: To
+	params?: never
+	query?: Routes[To]["query"]
+}
+
+interface BaseLinkPropsWithParams<To extends keyof Routes> {
+	to: To
+	params: Routes[To]["params"]
+	query?: Routes[To]["query"]
+}
+
+type BaseLinkProps<To extends keyof Routes> = Routes[To]["params"] extends Record<string, never>
+	? BaseLinkPropsWithoutParams<To>
+	: BaseLinkPropsWithParams<To>
+
+type LinkTypedProps<To extends keyof Routes> = Omit<LinkProps, "to"> & BaseLinkProps<To>
+
+export function LinkTyped<Route extends keyof Routes>({
+	params,
+	query,
+	...props
+}: LinkTypedProps<Route>) {
+	// @ts-expect-error
+	const to = params ? $path(props.to, params, query) : $path(props.to, query)
+	return <Link {...props} to={to} />
+}
+
+type NavLinkTypedProps<To extends keyof Routes> = Omit<NavLinkProps, "to"> & BaseLinkProps<To>
+
+export function NavLinkTyped({ params, query, ...props }: NavLinkTypedProps<keyof Routes>) {
+	// @ts-expect-error
+	const to = params ? $path(props.to, params, query) : $path(props.to, query)
+	return <NavLink {...props} to={to} />
+}
+
+export function useNavigateTyped() {
+	const navigate = useNavigate()
+	return <Route extends keyof Routes>(
+		...args: Route extends keyof Routes
+			? [to: Route, params?: Routes[Route]["params"], query?: Routes[Route]["query"]]
+			: [to: Route, query?: Routes[Route]["query"]]
+	) => {
+		navigate(
+			$path(
+				// the weird placement of this is to ensure only this error is suppressed
+				// @ts-expect-error
+				...args,
+			),
+		)
+	}
+}
+
+export function useParamsTyped<Route extends keyof RoutesWithParams>(route: Route) {
+	return $params(route, useParams())
+}
