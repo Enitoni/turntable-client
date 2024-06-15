4c3cdb5c8429e59c16d29f9145d895e35a266a18
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-06-15 15:14:10 -0500

    add fonts n stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,27 @@
 import type { Config } from "tailwindcss"
+import defaultTheme from "tailwindcss/defaultTheme.js"
 
 export default {
 	content: ["./app/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ["Lato", ...defaultTheme.fontFamily.sans],
+			display: ["Kufam", ...defaultTheme.fontFamily.sans],
+		},
+		boxShadow: {
+			sm: "2px 2px 7px -1px rgba(0, 0, 0, 0.13)",
+			DEFAULT: "4px 4px 10px 0 rgba(0, 0, 0, 0.1)",
+		},
+		extend: {
+			colors: {
+				primary: "#A845E4",
+				secondary: "#5471FF",
+				darkish: "#0A141D",
+				darkest: "#060A0E",
+				golden: "#FFBF1C",
+				invalid: "#EA3757",
+			},
+		},
 	},
 	plugins: [],
 } satisfies Config
