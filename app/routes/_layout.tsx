a502aa6f0179b17c61c324abfd4f7bbae2c6b0e3
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-09-03 12:42:18 +0200

    make page width a bit bigger

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -20,7 +20,7 @@ export default function Index() {
 	return (
 		<div>
 			<AppHeader user={user} />
-			<main className="max-w-[960px] mx-auto mt-16 px-4">
+			<main className="max-w-[1100px] mx-auto mt-16 px-4">
 				<Outlet />
 			</main>
 		</div>
