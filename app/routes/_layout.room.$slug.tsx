1283bf62b0fd8303b7cb8f39d2b31f94d2efda3d
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-09-03 12:44:11 +0200

    set up basic room page

---
+++ app/routes/_layout.room.$slug.tsx
@@ -0,0 +1,29 @@
+import { useLoaderData } from "@remix-run/react"
+import { Effect } from "effect"
+import { RoomHero } from "../features/room/RoomHero"
+import { getAuthorizedTurntableApi, resolveApiResponse } from "../lib/api"
+import { effectLoader, getParams } from "../lib/data"
+
+export const loader = effectLoader(
+	Effect.gen(function* () {
+		const api = yield* getAuthorizedTurntableApi()
+		const params = yield* getParams()
+		const room = yield* resolveApiResponse(api.rooms.room(params.slug as string))
+
+		return yield* Effect.succeed({ room })
+	}),
+)
+
+export default function Index() {
+	const { room } = useLoaderData<typeof loader>()
+
+	return (
+		<div>
+			<div className="mb-6">
+				<h1 className="title">{room.title}</h1>
+				<span className="text-base">{room.description}</span>
+			</div>
+			<RoomHero player={room.player} members={room.members} />
+		</div>
+	)
+}
