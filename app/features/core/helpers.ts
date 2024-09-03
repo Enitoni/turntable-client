43a36fca34c04e524c615db2d822f4c15235fe56
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-09-03 13:03:18 +0200

    add humanizeSeconds helper

---
+++ app/features/core/helpers.ts
@@ -0,0 +1,6 @@
+export function humanizeSeconds(seconds: number) {
+	const isMoreThanAnHour = seconds > 3600
+	const sliceStart = isMoreThanAnHour ? 11 : 14
+
+	return new Date(seconds * 1000).toISOString().slice(sliceStart, 19)
+}
