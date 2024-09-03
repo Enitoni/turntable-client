749c6a8fe5206df498f3c5f152b2200d6f5d7bbc
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-09-03 11:18:24 +0200

    add room list on index page

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -1,3 +1,29 @@
+import { useLoaderData } from "@remix-run/react"
+import { Effect } from "effect"
+import { RoomListItem } from "../features/room/RoomListItem"
+import { getAuthorizedTurntableApi, resolveApiResponse } from "../lib/api"
+import { effectLoader } from "../lib/data"
+
+export const loader = effectLoader(
+	Effect.gen(function* () {
+		const api = yield* getAuthorizedTurntableApi()
+		const rooms = yield* resolveApiResponse(api.rooms.listRooms())
+
+		return yield* Effect.succeed({ rooms })
+	}),
+)
+
 export default function Index() {
-	return <div>ass</div>
+	const { rooms } = useLoaderData<typeof loader>()
+
+	return (
+		<div>
+			<h1 className="mb-4 title">Rooms</h1>
+			<div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(24rem,1fr))]">
+				{rooms.map((room) => (
+					<RoomListItem key={room.id} room={room} />
+				))}
+			</div>
+		</div>
+	)
 }
