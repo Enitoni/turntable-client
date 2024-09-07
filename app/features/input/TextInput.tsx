e04535a461dd1e950d8a05b7a3979291626f1c52
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-09-07 03:01:59 +0200

    add necessary props to TextInput

--- app/features/input/TextInput.tsx
+++ app/features/input/TextInput.tsx
@@ -1,21 +1,25 @@
 export interface TextInputProps {
 	invalid?: boolean
 	defaultValue?: string
-	onInput?: (event: React.ChangeEvent) => void
+	value?: string
+	onInput?: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+	onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => void
 	rows?: number
 	type?: "text" | "password"
 	name?: string
 	placeholder?: string
+	disabled?: boolean
 }
 
 export function TextInput(props: TextInputProps) {
-	const { rows = 1, invalid, onInput, name, ...rest } = props
+	const { rows = 1, invalid, onInput, onKeyDown, name, ...rest } = props
 	const Element = rows > 1 ? "textarea" : "input"
 
 	return (
 		<Element
-			className="w-full h-10 px-3 leading-none transition border rounded-md outline-none bg-neutral-dark-100 placeholder:text-neutral-content-lighter border-neutral-light-100 hover:border-neutral-border-strong focus-within:outline-secondary"
+			className="w-full h-10 px-3 leading-none transition border rounded-md outline-none bg-neutral-dark-100 placeholder:text-neutral-content-lighter border-neutral-light-100 hover:border-neutral-border-strong focus-within:outline-secondary disabled:opacity-50"
 			onChange={onInput}
+			onKeyDown={onKeyDown}
 			name={name}
 			id={name}
 			{...rest}
