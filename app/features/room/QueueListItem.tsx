7ec2dae157e0a1da3c7db4232c90ee998ff50f46
Ilya <75043035+TheHamester@users.noreply.github.com>
2024-09-08 22:11:40 +0300

    added noopener noreffer

--- app/features/room/QueueListItem.tsx
+++ app/features/room/QueueListItem.tsx
@@ -12,12 +12,12 @@ export function QueueListItem(props: QueueListItemProps) {
 	return (
 		<div className="flex items-center gap-4">
 			<div className="border-2 rounded-md border-white/10 overflow-clip">
-				<a target="_blank" href={item.track.canonical}><TrackCover track={item.track} className="size-10" /></a>
+				<a target="_blank" href={item.track.canonical} rel="noopener noreferrer"><TrackCover track={item.track} className="size-10" /></a>
 			</div>
 			<div className="flex flex-col gap-1">
 				<div className="flex flex-col">
 					<span className="font-medium line-clamp-1 text-ellipsis">
-						<a target="_blank" href={item.track.canonical}>{item.track.artist} - {item.track.title}</a>
+						<a target="_blank" href={item.track.canonical} rel="noopener noreferrer">{item.track.artist} - {item.track.title}</a>
 					</span>
 					<span className="text-sm text-neutral-light-500 mt-[-0.1rem]">
 						{humanizeSeconds(item.track.duration)}
