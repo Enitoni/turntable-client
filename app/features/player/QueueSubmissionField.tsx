60d276486b1318c3031436769bfac2a1bbc27852
Enitoni <5295397+Enitoni@users.noreply.github.com>
2024-09-08 22:31:37 +0200

    show status when submitting to the queue

--- app/features/player/QueueSubmissionField.tsx
+++ app/features/player/QueueSubmissionField.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react"
+import { Check, X } from "lucide-react"
+import { useRef, useState } from "react"
+import { twMerge } from "tailwind-merge"
+import { Spinner } from "../core/Spinner"
 import { TextInput } from "../input/TextInput"
 import { addToQueue } from "../room/actions"
 
@@ -10,35 +13,93 @@ export function QueueSubmissionField(props: QueueSubmissionFieldProps) {
 	const { roomId } = props
 
 	const [input, setInput] = useState("")
-	const [isSubmitting, setIsSubmitting] = useState(false)
+
+	const timerRef = useRef(0)
+	const [status, setStatus] = useState<"idle" | "submitting" | "error" | "success">("idle")
+	const [isShown, setIsShown] = useState(false)
+	const [error, setError] = useState("")
 
 	const handleSubmit = (event: React.KeyboardEvent) => {
 		if (event.key === "Enter") {
 			event.preventDefault()
 
+			clearTimeout(timerRef.current)
+
 			if (input.length === 0) {
 				return
 			}
 
-			setIsSubmitting(true)
+			setStatus("submitting")
+			setIsShown(true)
 
-			addToQueue(roomId, input)
-				.then((response) => {
-					if (response.ok) {
-						setInput("")
-					}
-				})
-				.finally(() => setIsSubmitting(false))
+			submit().finally(() => {
+				timerRef.current = window.setTimeout(() => {
+					setIsShown(false)
+				}, 5000)
+			})
 		}
 	}
 
+	const submit = async () => {
+		const response = await addToQueue(roomId, input)
+		const json = await response.json()
+
+		if (response.ok) {
+			setInput("")
+			setStatus("success")
+		} else {
+			setStatus("error")
+			setError(json.message)
+		}
+	}
+
+	const renderStatus = () => {
+		if (status === "error") {
+			return (
+				<div className="flex items-center h-full gap-2 text-invalid">
+					<X />
+					<span className="-mt-0.5">{error}</span>
+				</div>
+			)
+		}
+
+		if (status === "success") {
+			return (
+				<div className="flex items-center h-full gap-2 text-success">
+					<Check />
+					<span className="-mt-0.5">Added successfully</span>
+				</div>
+			)
+		}
+
+		return (
+			<div className="flex items-center h-full gap-4">
+				<Spinner className="size-4" />
+				<span className="-mt-0.5">Submitting...</span>
+			</div>
+		)
+	}
+
 	return (
-		<TextInput
-			placeholder="Paste a link to add to queue"
-			value={input}
-			disabled={isSubmitting}
-			onInput={(event) => setInput(event.target.value)}
-			onKeyDown={handleSubmit}
-		/>
+		<div className="relative w-full">
+			<div
+				className={twMerge(
+					"absolute h-12 top-[-92px] left-0 card px-4 transition translate-y-2 opacity-0",
+					isShown && "translate-y-0 opacity-100",
+				)}
+			>
+				<div className="absolute top-[47px] ">
+					<div className="triangle-down" />
+				</div>
+				{renderStatus()}
+			</div>
+			<TextInput
+				placeholder="Paste a link to add to queue"
+				value={input}
+				disabled={status === "submitting"}
+				onInput={(event) => setInput(event.target.value)}
+				onKeyDown={handleSubmit}
+			/>
+		</div>
 	)
 }
